3ac8265b2f4fee3c90d06d05f1536d84dcf6ad6b
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2025-05-20 13:05:00 +0900

    first commit

---
+++ app/src/main/java/people/count/Info.java
@@ -0,0 +1,16 @@
+package people.count;
+
+public class Info {
+    // String kafka1 = "10.3.128.33:60011";
+    // String kafka2 = "10.3.128.33:60012";
+    // String kafka3 = "10.3.128.33:60013";
+    String kafka1 = "192.168.99.47:9092";
+    String kafka2 = "192.168.99.48:9092";
+    String kafka3 = "192.168.99.49:9092";
+    //String topicname = "people-count-event";
+    String topicname = "pose-classification-event";
+    String url = "mongodb://root:gamelab2)5@10.3.128.35:14407/";
+    String database = "people-count-event";
+    String people_collection = "people-count-event";
+    String roi_collection = "roi-count-event";
+}
